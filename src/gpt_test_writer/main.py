8fe0d3ae0ad95b1ac1e72670969a0e0d8775f007
Kasper Janehag <kasper_janehag@mckinsey.com>
2023-11-14 10:32:10 +0100

    Add packages

---
+++ src/gpt_test_writer/main.py
@@ -0,0 +1,6 @@
+def add(first_number: int, second_number: int):
+    return first_number + second_number
+
+
+def subtract(first_number: int, second_number: int):
+    return first_number - second_number
